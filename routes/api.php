456e2daab0caf46d1fec224b4e4e62b5959b5175
AHMAD FAUZAN ANTONI <fauzanantoniahmad@gmail.com>
2024-09-26 13:56:52 +0700

    K

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Route;
+use Api\ArtistController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,3 +17,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('artist',ArtistController::class);
+//Route::apiResource('album', \App\Http\Controllers\Api\AlbumController::class);
+//Route::apiResource('event', \App\Http\Controllers\Api\EventController::class);
+//Route::apiResource('merchandise', \App\Http\Controllers\Api\MerchandiseController::class);
+//Route::apiResource('song', \App\Http\Controllers\Api\SongController::class);
+//Route::apiResource('testimonial', \App\Http\Controllers\Api\TestimonialController::class);
